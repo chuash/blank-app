cefafc11b6bddb202c458c7e3b66d4e0dc24126e
chuash <seowhow@gmail.com>
2024-10-03 16:30:12 +0000

    First Streamlit Cloud Commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        "Get help": "https://www.google.com",
+        "About": "https://www.cccs.gov.sg/",
+    },
 )
+
+st.title("Uber pickups in NYC")
+st.text_input("Your name, please", key="nameholder")
+st.write(f"Hello, {st.session_state.nameholder} \n")
+
+add_slider = st.sidebar.slider("Select a range of values", 0.0, 100.0, (25.0, 75.0))
